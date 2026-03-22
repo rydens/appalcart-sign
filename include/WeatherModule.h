478c0dda87dbf2f0cd836010af1a0a1fdaaf75d9
Narwar04 <carenathan027@gmail.com>
2026-03-22 01:32:11 -0400

    Weather Module should be done, needs to be tested/properly organized before merging to main

---
+++ include/WeatherModule.h
@@ -0,0 +1,36 @@
+#ifndef WEATHER_MODULE_H
+#define WEATHER_MODULE_H
+
+#include "cpr/cpr.h"
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <string>
+#include "Module.h"
+
+using json = nlohmann::json;
+
+const std::string FORCAST_URL = "https://api.weather.gov/gridpoints/RNK/17,16/forecast/hourly"
+
+typedef struct {
+    int temperature;
+    std::string temperatureUnit;
+    std::string windSpeed;
+    std::string windDirection;
+    std::string forcast;
+    int preciptaionPercetage;
+    int humidityPercentage;
+} Forcast_t;
+
+class WeatherModule : public Module {
+    private:
+        Forcast_t currentForecast;
+
+        Forcast_t parseForecast(const cpr::Response& res)
+    public:
+        void execute();
+        int render(rgb_matrix::Canvas * canvas, int x, int y, int height, int width);
+        // to get currnet forcast form outside
+        Forcast_t getForcast() const { return currentForecast}
+};  
+
+#endif
---
+++ src/WeatherModule.cpp
@@ -0,0 +1,53 @@
+#include "weatherParser.h"
+
+WeatherModule::WeatherModule() : currentForecast{} {}
+
+Forcast_t WeatherModule::parseForecast(){
+    Forcast_t result{};
+
+    if(res.status_code != 200) {
+        std::cerr << "Request Failed: " << res.status_code << "\n";
+        return result;
+    }
+
+    json data = json::parse(res.text);
+
+    // get into the location of most values
+    if(!data.contains("properties") ||
+        !data["properties"].contains("periods") ||
+        data["properties"]["periods"].empty()) {
+            std::cerr << "No Forcast data found\n";
+            return result;
+        }
+
+    const json& period = data["properties"]["periods"][0]
+
+    result.temperature = period.value("temperature", 0);
+    result.temperatureUnit = period.value("temperatureUnit", "");
+    result.windSpeed = period.value("windSpeed", "");
+    result.windDirection = period.value("windDirection", "");
+    result.forcast = period.value("shortForcast", "");
+
+    if(!data.contains("probabilityOfPrecipitation") ||
+        !data["probabilityOfPrecipitation"]["value"].is_null()){
+            result.preciptaionPercetage = period["probabilityOfPrecipitation"].value("value",0);
+    }
+
+    if(!data.contains("relativeHumidity") ||
+        !data["relativeHumidity"]["value"].is_null()){
+            result.relativeHumidity = period["relativeHumidity"].value("value",0);
+    }
+
+    return result
+}
+
+void WeatherModule::execute(){
+     cpr::Response res = cpr::Get(cpr::Url{FORECAST_URL});
+
+     currentForecast = parseForecast(res);
+
+}
+
+int WeatherModule::render(rgb_matrix::Canvas * canvas, int x, int y, int height, int width){
+    // render function needs the hand of blaez
+}
